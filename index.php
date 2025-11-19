30d12e8a5b0750d4483c85bc312a977f7f4ac004
Zunanda <zuzukindi@gmail.com>
2025-11-19 09:15:54 +0700

    Implement search feature in alumni data display
    Added search functionality and updated table structure.

--- index.php
+++ index.php
@@ -4,14 +4,23 @@
 
 <head>
     <title>Data Alumni</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="stylee.css">
 </head>
 
 <body>
     <h2>Data Alumni Sekolah</h2>
-    <a href="tambah.php">+ Tambah Data</a>
+    <a href="tambah.php" id="tambahdata">+ Tambah Data</a>
+
+    <!-- FORM PENCARIAN -->
+    <form method="GET" style="margin-bottom: 20px;">
+        <input type="text" name="cari" placeholder="Cari nama / NIK / NISN / Jurusan..."
+            value="<?= isset($_GET['cari']) ? $_GET['cari'] : '' ?>">
+        <button type="submit">Cari</button>
+    </form>
+
     <table>
         <tr>
+            <th>ID</th>
             <th>Nama</th>
             <th>NIK</th>
             <th>NISN</th>
@@ -20,25 +29,40 @@
             <th>Alamat</th>
             <th>Tahun Lulus</th>
             <th>Jurusan</th>
-            <th>ubah</th>
+            <th>Perubahan</th>
         </tr>
+
         <?php
-        $result = mysqli_query($conn, "SELECT * FROM alumni");
+        // PENCARIAN
+        if (isset($_GET['cari'])) {
+            $cari = $_GET['cari'];
+            $result = mysqli_query($conn, "SELECT * FROM alumni 
+                WHERE nama LIKE '%$cari%' 
+                OR nik LIKE '%$cari%' 
+                OR nisn LIKE '%$cari%' 
+                OR jurusan LIKE '%$cari%' 
+                OR tahun_lulus LIKE '%$cari%' ");
+        } else {
+            $result = mysqli_query($conn, "SELECT * FROM alumni");
+        }
+
+        // TAMPILKAN DATA
         while ($row = mysqli_fetch_assoc($result)) {
             echo "<tr>
-        <td>{$row['nama']}</td>
-        <td>{$row['nik']}</td>
-        <td>{$row['nisn']}</td>
-        <td>{$row['tempat_lahir']}</td>
-        <td>{$row['tanggal_lahir']}</td>
-        <td>{$row['alamat']}</td>
-        <td>{$row['tahun_lulus']}</td>
-        <td>{$row['jurusan']}</td>
-        <td>
-          <a href='edit.php?id={$row['id']}'>Edit</a> |
-          <a href='hapus.php?id={$row['id']}' onclick=\"return confirm('Yakin ingin hapus?')\">Hapus</a>
-        </td>
-      </tr>";
+                <td>{$row['id']}</td>
+                <td>{$row['nama']}</td>
+                <td>{$row['nik']}</td>
+                <td>{$row['nisn']}</td>
+                <td>{$row['tempat_lahir']}</td>
+                <td>{$row['tanggal_lahir']}</td>
+                <td>{$row['alamat']}</td>
+                <td>{$row['tahun_lulus']}</td>
+                <td>{$row['jurusan']}</td>
+                <td>
+                    <a href='edit.php?id={$row['id']}'>Edit</a> |
+                    <a href='hapus.php?id={$row['id']}' onclick=\"return confirm('Yakin ingin hapus?')\">Hapus</a>
+                </td>
+            </tr>";
         }
         ?>
     </table>
